e93c64372faa92979a34cd080c473cfa0ef86242
MeRaj Rudba <mrudba@gmail.com>
2020-03-06 12:59:51 +0600

    Null vs Undefined Done

---
+++ true-false.js
@@ -0,0 +1,9 @@
+const name ="Solaiman";
+
+if(name.length>0){
+    console.log("condition is true");
+
+}
+else{
+    console.log("Condition is false")
+}
---
+++ undefined-null.js
@@ -0,0 +1,33 @@
+//UNDEFINED
+//type1
+let pakhi;
+console.log(pakhi);
+//type2
+function add(num1,num2){
+    console.log(num1+num2);
+}
+
+const result = add(13,82);
+console.log(result);
+
+//type3
+function add2(num1,num2){
+    console.log(num1,num2);
+}
+const result2=add2(13);
+console.log(result2);
+
+//type4
+const premik={name:"Smart Dude", phone:45684 };
+console.log(premik.gf);
+
+//type5
+let fun = undefined;
+console.log(fun);
+
+
+
+//NULL
+//type1
+let ages=[2,5,9]
+console.log(ages[11]);
